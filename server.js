ff39c0e2582cd4775cabd026ef9fc5a3077964c0
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-20 19:34:38 +0800

    reset

--- server.js
+++ server.js
@@ -1,17 +1,60 @@
-const express = require('express')
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const app = express();
-// app configuration
-app.set('port', (process.env.PORT || 3000));
-// setup our express application
-app.use(morgan('dev')); // log every request to the console.
-app.use(bodyParser.urlencoded({ extended:false }));
-app.use(bodyParser.json()); 
-// app routes
-require('./routes/webhook_verify')(app);
-// warming up the engines !! setta !! go !!!.
-app.listen(app.get('port'), function() {
-  const url = 'http://localhost:' + app.set('port');
-  console.log('Application running on port: ', app.get('port'));
+'use strict'
+
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json());
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
